067bc269f283d6fc0665e1b5964e15deb763ad7f
Matt Saalfeld <nestharken@gmail.com>
2015-03-03 00:07:53 -0500

    Add a test for paths.

---
+++ src/test/groovy/PathSpec.groovy
@@ -0,0 +1,29 @@
+import spock.lang.*
+
+class PathSpec extends Specification {
+
+	@Shared vertexA
+	@Shared vertexB
+	@Shared vertexC
+	@shared edgeAB
+	@shared edgeBC
+	@Shared path
+
+	void setup() {
+		vertexA = new Vertex("A")
+		vertexB = new Vertex("B")
+		vertexC = new Vertex("C")
+		edgeAB = new Edge(first:vertexA, second: vertexB, distance: 3)
+		edgeBC = new Edge(first:vertexB, second: vertexC, distance: 5)
+		path.edges.add(edgeAB)
+		path.edges.add(edgeBC)
+	}
+
+	void "a path should return the correct total distance"() {
+		assert path.distance == 8
+	}
+
+	void "a path should return the correct vertices"() {
+		assert path.vertices == [vertexA, vertexB, vertexC]
+	}
+}
