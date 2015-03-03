d285ae1978d2f50d3f1c6a75c029e37dbbe87337
Matt Saalfeld <nestharken@gmail.com>
2015-03-03 01:14:16 -0500

    Implement the other side of an edge.

--- src/main/groovy/Edge.groovy
+++ src/main/groovy/Edge.groovy
@@ -2,4 +2,14 @@ class Edge {
 	int distance
 	Vertex first
 	Vertex second
+
+	def other(vertex) {
+		if (vertex == first) {
+			return second
+		} else if (vertex == second) {
+			return first
+		} else {
+			return null
+		}
+	}
 }
