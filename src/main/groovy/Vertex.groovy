a33d4ba1ee06be1b2d6c0d2ca1dc34ae5ccede1f
Matt Saalfeld <nestharken@gmail.com>
2015-03-03 01:46:46 -0500

    Implemented path finding.

--- src/main/groovy/Vertex.groovy
+++ src/main/groovy/Vertex.groovy
@@ -5,4 +5,20 @@ class Vertex {
 	Vertex(name) {
 		this.name = name
 	}
+
+	def findPathTo(def destination, def previous = null) {
+		def path
+		for (edge in edges) {
+			def other = edge.other(this)
+			if (other == destination) {
+				path = new Path()
+			} else if (other != previous) {
+				path = other.findPathTo(destination, this)
+			}
+			if (path != null) {
+				path.edges = [edge] + path.edges
+				return path
+			}
+		}
+	}
 }
--- src/test/groovy/VertexSpec.groovy
+++ src/test/groovy/VertexSpec.groovy
@@ -4,18 +4,25 @@ class VertexSpec extends Specification {
 	@Shared vertexA
 	@Shared vertexB
 	@Shared vertexC
+	@Shared vertexD
 	@Shared edgeAB
 	@Shared edgeAC
-
+	@Shared edgeBD
 
 	void setup() {
 		vertexA = new Vertex("A")
 		vertexB = new Vertex("B")
 		vertexC = new Vertex("C")
+		vertexD = new Vertex("D")
 		edgeAB = new Edge(first:vertexA, second: vertexB, distance: 3)
 		edgeAC = new Edge(first:vertexA, second: vertexC, distance: 5)
+		edgeBD = new Edge(first:vertexB, second: vertexD, distance: 6)
 		vertexA.edges.add(edgeAB)
+		vertexB.edges.add(edgeAB)
 		vertexA.edges.add(edgeAC)
+		vertexC.edges.add(edgeAC)
+		vertexB.edges.add(edgeBD)
+		vertexD.edges.add(edgeBD)
 	}
 
 	def "a vertex should return its name"() {
@@ -37,8 +44,8 @@ class VertexSpec extends Specification {
 	def "a vertex should be able to find a path"() {
 		when:
 			setup()
-			def foundPath = a.findPathTo(c)
+			def foundPath = vertexA.findPathTo(vertexD)
 		then:
-			foundPath.edges = path.edges
+			foundPath.edges == [edgeAB, edgeBD]
 	}
 }
